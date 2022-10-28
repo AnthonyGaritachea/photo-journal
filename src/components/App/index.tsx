f457c45e7f66846eb55ac1ea8bb99ccd8d2d8b01
anthonygaritachea <lgaritachea45204@gmail.com>
2022-10-27 21:04:12 -0700

    adds eslint and fixes all linter errors

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,11 @@
+import React from 'react'
+
 const App: React.FC = () => {
-    return (
-        <>
-            App.js 
-        </>
+ return (
+        <div>
+            <p>app.js</p>
+        </div>
     )
-};
+}
 
 export default App
